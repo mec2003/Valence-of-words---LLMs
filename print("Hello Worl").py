12a885792b696d171bef31256801f5d69f5762b8
Maria Eduarda de Carvalho <mariacarvalho.20231@poli.ufrj.br>
2024-09-12 22:51:41 -0300

    rascunho

--- print("Hello Worl").py
+++ print("Hello Worl").py
@@ -1,1 +1,17 @@
-print("Hello Worl")
+#import matplotlib.pyplot as plt
+
+#x = []
+#y = []
+
+#with open('dados.txt', 'r') as file:
+    #for line in file: # Dividir a linha em dois números (x, y)
+        
+        #values = line.split()
+        #x.append(float(values[0]))  # Adicionar o valor de x na lista
+        #y.append(float(values[1]))  # Adicionar o valor de y na lista
+
+# Criar o gráfico
+#plt.plot(x, y)
+
+# Exibir o gráfico
+#plt.show()
